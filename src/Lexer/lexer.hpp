a87843852cc7d5036f38fe8f970d8191566af1a3
Brandon's Zenbook <tangbrandonk@gmail.com>
2024-07-08 12:46:43 -0700

    Updated files.

--- src/Lexer/lexer.hpp
+++ src/Lexer/lexer.hpp
@@ -2,5 +2,8 @@
 #define LEXER_HPP
 
 #include <regex>
+#include "../globals.hpp"
+
+Lexer lex;
 
 #endif
--- src/globals.hpp
+++ src/globals.hpp
@@ -5,4 +5,31 @@
 #include <vector>
 #include <string>
 
+enum TokenType
+{
+    KEYWORD,
+    IDENTIFIER,
+    NUMBER,
+    STRING,
+    OPERATOR,
+    DELIMITER,
+    COMMENT,
+    UNKNOWN
+};
+
+struct Token
+{
+    TokenType type;
+    std::string text;
+};
+
+class Lexer {
+    private:
+        
+    public:
+        std::vector<Token> tokenized_code;
+};
+
+extern Lexer lex;
+
 #endif
